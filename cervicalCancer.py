fa5fa68010fc598c0f62a6474903e352450c2dad
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-16 20:17:25 +0800

    Update cervicalCancer.py

--- cervicalCancer.py
+++ cervicalCancer.py
@@ -101,5 +101,5 @@ def check_diagnosis(hinselmann, schiller, citology):
         return rule['diagnosis']
     return 'No diagnosis available'
 
-if name == 'main':
+if __name__ == __'main'__:
     main()
