6744eab8e2ab5d752191f4bcb7259c5632aab8cb
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-16 10:33:51 +0800

    Update cervicalCancer.py

--- cervicalCancer.py
+++ cervicalCancer.py
@@ -14,8 +14,18 @@ df = load_dataset()
 def cervical_cancer_diagnosis(symptoms):
     # Define the rules for cervical cancer diagnosis
     rules = {
-        'rule1': {'Bleeding': '1', 'Smoking': '1', 'Hormonal Contraceptives': '1', 'Diagnosis': 'Positive'},
-        'rule2': {'Bleeding': '0', 'Smoking': '0', 'Hormonal Contraceptives': '0', 'Diagnosis': 'Negative'},
+        'rule1': {'Age': '<30', 'Number of sexual partners': '>2', 'First sexual intercourse': '<18', 'Num of pregnancies': '>0',
+                  'Smokes': 'Yes', 'Smokes (years)': '>5', 'Hormonal Contraceptives': 'Yes', 'Hormonal Contraceptives (years)': '>5',
+                  'IUD': 'No', 'STDs': 'Yes', 'STDs (number)': '>0', 'STDs:condylomatosis': 'No',
+                  'STDs:cervical condylomatosis': 'No', 'STDs: Number of diagnosis': '>0',
+                  'STDs: Time since first diagnosis': '>1', 'Dx:Cancer': 'Yes', 'Dx:CIN': 'Yes',
+                  'Dx': 'Yes', 'Hinselmann': '1', 'Citology': '1', 'Biopsy': '1', 'Diagnosis': 'Positive'},
+        'rule2': {'Age': '>=30', 'Number of sexual partners': '<=2', 'First sexual intercourse': '>=18', 'Num of pregnancies': '0',
+                  'Smokes': 'No', 'Smokes (years)': '0', 'Hormonal Contraceptives': 'No', 'Hormonal Contraceptives (years)': '0',
+                  'IUD': 'No', 'STDs': 'No', 'STDs (number)': '0', 'STDs:condylomatosis': 'No',
+                  'STDs:cervical condylomatosis': 'No', 'STDs: Number of diagnosis': '0',
+                  'STDs: Time since first diagnosis': '0', 'Dx:Cancer': 'No', 'Dx:CIN': 'No',
+                  'Dx': 'No', 'Hinselmann': '0', 'Citology': '0', 'Biopsy': '0', 'Diagnosis': 'Negative'},
         # Add more rules as per your requirements
     }
 
@@ -23,7 +33,7 @@ def cervical_cancer_diagnosis(symptoms):
     for rule, conditions in rules.items():
         match = True
         for symptom, value in conditions.items():
-            if symptoms[symptom] != value:
+            if symptom not in symptoms or symptoms[symptom] != value:
                 match = False
                 break
         if match:
@@ -31,13 +41,6 @@ def cervical_cancer_diagnosis(symptoms):
     
     return 'Unknown'  # Return 'Unknown' if no matching rule is found
 
-# Define the interrelated questions for the questionnaire
-questions = {
-    'Bleeding': 'Have you experienced any abnormal vaginal bleeding?',
-    'Smoking': 'Do you smoke?',
-    'Hormonal Contraceptives': 'Have you used hormonal contraceptives?',
-    # Add more questions as per your requirements
-}
 
 # Main Streamlit code
 def main():
@@ -56,12 +59,16 @@ def main():
     # Perform the diagnosis
     diagnosis = cervical_cancer_diagnosis(symptoms)
 
-    # Display the diagnosis result
+    # Display the diagnosis result with different colors
     st.subheader('Diagnosis Result')
     if diagnosis == 'Positive':
-        st.write('Based on your symptoms, the diagnosis suggests **positive** for cervical cancer. Please consult a healthcare professional for further evaluation.')
+        st.write('Based on your symptoms, the diagnosis suggests **positive** for cervical cancer. Please consult a healthcare professional for further evaluation.',
+                 unsafe_allow_html=True)
+        st.markdown('<style>div.stButton > button:first-child {background-color: red;}</style>', unsafe_allow_html=True)
     elif diagnosis == 'Negative':
-        st.write('Based on your symptoms, the diagnosis suggests **negative** for cervical cancer. However, it is always advisable to consult a healthcare professional for confirmation.')
+        st.write('Based on your symptoms, the diagnosis suggests **negative** for cervical cancer. However, it is always advisable to consult a healthcare professional for confirmation.',
+                 unsafe_allow_html=True)
+        st.markdown('<style>div.stButton > button:first-child {background-color: yellow;}</style>', unsafe_allow_html=True)
     else:
         st.write('The diagnosis result is **unknown**. Please consult a healthcare professional for proper evaluation.')
 
